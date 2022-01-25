6efa40816305f31196c64a5ed0630507890da81b
jbenga <jpaul.benga@gmail.com>
2022-01-25 11:13:03 +0100

    Exercice 4

--- exam/src/app/app.component.ts
+++ exam/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './modeles/statistique';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exam';
+  tabStats:Array<Statistique> = [new Statistique ("number1","pokemon rouge","151"),
+new Statistique ("number2","tekken7","8P")];
 }
+
