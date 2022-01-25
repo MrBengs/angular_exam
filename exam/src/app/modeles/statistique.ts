6094e56986d2f2a239b52ad01904c37f0b35e246
jbenga <jpaul.benga@gmail.com>
2022-01-25 10:44:04 +0100

    exercice 3

---
+++ exam/src/app/modeles/statistique.ts
@@ -0,0 +1,11 @@
+export class Statistique{
+    id:string;
+    title:string;
+    value:string;
+
+    constructor(id:string, title:string, value:string){
+        this.id=id;
+        this.title=title;
+        this.value=value;
+    }
+}
